1ea70c4d574f56287ae61b30141cd65411f55045
Matt Arch Linux <mattwalker340@gmail.com>
2020-12-26 16:27:03 +0000

    Removed terminator from intsall script as now using alacritty

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
   cp ~/.dotfiles/Monaco\ Nerd\ Font\ Complete\ Mono.otf ~/.local/share/fonts
   sudo fc-cache -fv
   ln -s ~/.dotfiles/arch/.zshrc ~/.zshrc
-  mkdir -p ~/.config/{i3,terminator,redshift}
+  mkdir -p ~/.config/{i3,redshift}
   systemctl --user enable redshift.service
   ln -s ~/.dotfiles/arch/.config/redshift.conf ~/.config/redshift/redshift.conf
 elif [[ "$unamestr" == 'Darwin' ]]; then
@@ -37,13 +37,6 @@ ln -s ~/.dotfiles/arch/.xinitrc ~/.xinitrc
 ln -s ~/.dotfiles/arch/.Xresources ~/.Xresources
 ln -s ~/.dotfiles/arch/i3/config ~/.config/i3
 ln -s ~/.dotfiles/arch/i3/i3blocks.conf ~/.config/i3
-ln -s ~/.dotfiles/arch/terminator/config ~/.config/terminator
-
-# Other
-
-# ZSH plugins
-#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Set the global gitignore
 git config --global core.excludesfile ~/.dotfiles/.gitignore_global
