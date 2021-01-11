ea7640dc4780e66f3063807e6b14c7c8f5b2817d
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-11 19:35:50 +0000

    Added shell script for putting spotify song in the i3 bar

---
+++ arch/i3/spotify_song.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+metadata=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+
+song=$(grep xesam:title -A 1 <<< "$metadata" | tail -n 1)
+song=${song#*\"}
+song=${song%\"*}
+
+artist=$(grep xesam:artist -A 2 <<< "$metadata" | tail -n 1)
+artist=${artist#*\"}
+artist=${artist%\"*}
+
+# full text, short text (colour and bg colour omitted)
+echo "♫ $song/$artist"
+echo "$song"
