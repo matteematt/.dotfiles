178a6db86bf0787ef9a8cec64d0a11ba2f62834c
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-11 19:49:29 +0000

    Spotify shell script now handles spotify being closed and hides when music is paused

--- arch/i3/spotify_song.sh
+++ arch/i3/spotify_song.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-metadata=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+metadata=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata') || exit 0
 
-song=$(grep xesam:title -A 1 <<< "$metadata" | tail -n 1)
-song=${song#*\"}
-song=${song%\"*}
+if [[ "$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus')" =~ "Playing" ]];then
+  song=$(grep xesam:title -A 1 <<< "$metadata" | tail -n 1)
+  song=${song#*\"}
+  song=${song%\"*}
 
-artist=$(grep xesam:artist -A 2 <<< "$metadata" | tail -n 1)
-artist=${artist#*\"}
-artist=${artist%\"*}
+  artist=$(grep xesam:artist -A 2 <<< "$metadata" | tail -n 1)
+  artist=${artist#*\"}
+  artist=${artist%\"*}
 
-# full text, short text (colour and bg colour omitted)
-echo "♫ $song/$artist"
-echo "$song"
+  # full text, short text (colour and bg colour omitted)
+  echo "♫ $song/$artist"
+  echo "$song"
+fi
+
