7665f8a463754cc460eadde86c009048fe2546aa
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-12 18:49:46 +0000

    Improved spotify script, reducing sub-processes

--- arch/i3/spotify_song.sh
+++ arch/i3/spotify_song.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-metadata=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata') || exit 0
+args=(--print-reply \
+  --session \
+  --dest=org.mpris.MediaPlayer2.spotify \
+  /org/mpris/MediaPlayer2 \
+  org.freedesktop.DBus.Properties.Get \
+  string:'org.mpris.MediaPlayer2.Player')
 
-if [[ "$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus')" =~ "Playing" ]];then
-  song=$(grep xesam:title -A 1 <<< "$metadata" | tail -n 1)
-  song=${song#*\"}
-  song=${song%\"*}
+artist_loc=27
+song_loc=40
 
-  artist=$(grep xesam:artist -A 2 <<< "$metadata" | tail -n 1)
-  artist=${artist#*\"}
+if [[ $(eval "dbus-send ${args[@]} string:'PlaybackStatus' 2> /dev/null") =~ "Playing" ]];then
+  readarray metadata < <(eval "dbus-send ${args[@]} string:'Metadata'")
+  artist=${metadata[$artist_loc]#*\"}
   artist=${artist%\"*}
+
+  song=${metadata[$song_loc]#*\"}
+  song=${song%\"*}
 
   # full text, short text (colour and bg colour omitted)
   echo "♫ $song/$artist"
