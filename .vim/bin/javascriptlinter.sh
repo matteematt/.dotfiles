33dc0c2c59ed44e98bb9e0bcb11758e7928d0beb
Matt Walker <43502076+matteematt@users.noreply.github.com>
2021-03-22 20:35:10 +0000

    Eslint setup (#8)
    * Updated spelling file
    
    * Updated spelling file
    
    * Created wrapper script for eslint so it can search backwards for config
    
    * Set other linting wrapper scripts to use #!/bin/bash

---
+++ .vim/bin/javascriptlinter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+config_file=".eslintrc"
+file_extensions=("" ".js" ".json")
+
+function configPath() {
+	local config_file_path=$(realpath "$(pwd)/$1/$config_file")
+	for ext in "${file_extensions[@]}"; do
+		[[ -f "$config_file_path$ext" ]] && echo "$config_file_path$ext" && return 0
+	done
+	[[ "$config_file_path" == "/.eslintrc" ]] && return 1
+	configPath "$1/.."
+}
+
+# Get the config file path (and bail on failure)
+config_file_path=$(configPath "") || exit 1
+
+case $# in
+	0)
+		# Checking that eslint is executable
+		hash eslint 1> /dev/null || exit 1
+		;;
+	1)
+		# Run the linter in error reporting mode, filename is $1
+		eslint --format unix "$1"
+		;;
+	2)
+		# Run linter in fix mode, filename is $2
+		eslint --fix "$2"
+		;;
+esac
