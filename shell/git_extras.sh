d934f758049159d526ce67f50b54d1c21d18e848
Matt Walker <matthew.walker@sky.uk>
2021-04-01 08:28:26 +0100

    Fixed linting issues

--- shell/git_extras.sh
+++ shell/git_extras.sh
@@ -14,9 +14,9 @@
 # D x - for untracked directory x
 # for all *unstaged* files
 # returns exit code 1 if this is not a git directory
-function __formatGitStatus {
-  git branch --show-current &>/dev/null || {echo "Error: not a git directory";return 1}
-  formatted=`git status | awk 'BEGIN {parseMode=0} \
+function __formatGitStatus() {
+  git branch --show-current &>/dev/null || { echo "Error: not a git directory";return 1; }
+  formatted=$(git status | awk 'BEGIN {parseMode=0} \
   { \
     if (parseMode==1) { \
       if (match($0,/^\s+deleted:\s*(.+)/)) {print "R " $2}; \
@@ -28,36 +28,36 @@ function __formatGitStatus {
     }; \
     if ($0 ~ /Changes not staged for commit/) {parseMode=1}; \
     if ($0 ~ /to include in what will be committed/) {parseMode=2}; \
-  }'`
+  }')
   echo "$formatted"
 }
 
 # Similar to getDiffByList but views the output in bat inline and
 # selecting an option automatically calls 'git add' on it
-function gitViewAndStage {
-  chosen_file=`__formatGitStatus | fzf --with-nth 2 --preview '~/.dotfiles/shell/view_git_unstaged_file.sh {}'`
+function gitViewAndStage() {
+  chosen_file=$(__formatGitStatus | fzf --with-nth 2 --preview '$HOME/.dotfiles/shell/view_git_unstaged_file.sh {}')
   if [ -z "$chosen_file" ]; then
     return
   else
-    git add `echo "$chosen_file" | cut -d" " -f2`
+    git add $(echo "$chosen_file" | cut -d" " -f2)
     unset chosen_file
   fi
 }
 
-function getDiffByList {
-  chosen_file=`__formatGitStatus | fzf --with-nth 2`
+function getDiffByList() {
+  chosen_file=$(__formatGitStatus | fzf --with-nth 2)
   if [ -z "$chosen_file" ]; then
     return
   fi
-  file_path=`echo "$chosen_file" | cut -d" " -f2`
+  file_path=$(echo "$chosen_file" | cut -d" " -f2)
   ~/.dotfiles/shell/view_git_unstaged_file.sh "$chosen_file"
   unset chosen_file
 }
 
-function addLastDiffFile {
+function addLastDiffFile() {
   if [[ -v file_path ]];
   then
-    git add $file_path
+    git add "$file_path"
     unset file_path
   else
     echo "No previous file diffed"
@@ -67,12 +67,12 @@ function addLastDiffFile {
 
 # Performs the same job as the update button on giuthub when a branch
 # is behind master, but with rebase instead
-function getUpdateWithRebase {
+function getUpdateWithRebase() {
   branch=$(git rev-parse --abbrev-ref HEAD)
   git pull
   git checkout master
   git pull
-  git checkout $branch
+  git checkout "$branch"
   git rebase origin/master
   git push --force-with-lease
 }
