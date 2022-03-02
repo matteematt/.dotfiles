5f601ba2cd85ee198806c947e1993c83dce65cf9
Matt Walker <matthew.walker@sky.uk>
2022-03-02 13:37:57 +0000

    Added (broken) start screen for nvim

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -9,3 +9,7 @@ vim.g.maplocalleader = " "
 
 -- Open the current file location in netrw
 kmap('n', '<leader>w', '<C-w>v<C-w>l', opts)
+
+-- Make jumps go to the middle of the screen and open any folds
+kmap('n', 'n', 'nzzzv', opts)
+kmap('n', 'N', 'Nzzzv', opts)
