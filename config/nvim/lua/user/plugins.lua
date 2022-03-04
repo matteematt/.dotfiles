479e5853cdecbb58da0370ce857be651a8e995b4
Matt Walker <matthew.walker@sky.uk>
2022-03-04 10:05:58 +0000

    Format the plugin.lua file

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -1,81 +1,90 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system(
-		{'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path}
-	)
-	print "Installing packer close and open Neovim..."
-	vim.cmd [[packadd packer.nvim]]
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and open Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Every time we update this file, rerun packer to update the plugins
-vim.cmd [[
+vim.cmd([[
 	augroup packer_user_config
 		autocmd!
 		autocmd BufWritePost plugins.lua source <afile> | PackerSync
 	augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	print "Unable to load packer, lua/user/plugins.lua"
+	print("Unable to load packer, lua/user/plugins.lua")
 	return
 end
 
 -- Have packer use a popup window
-packer.init {
+packer.init({
 	display = {
 		open_fn = function()
-			return require("packer.util").float { border = "rounded" }
+			return require("packer.util").float({ border = "rounded" })
 		end,
 	},
-}
+})
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
 	-- Required for other plugins
-	use 'nvim-lua/popup.nvim'
-	use 'nvim-lua/plenary.nvim'
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
 
 	-- cmp plugins
-	use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-buffer"
-	use "hrsh7th/cmp-path"
-	use "hrsh7th/cmp-cmdline"
-	use "saadparwaiz1/cmp_luasnip"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/cmp-nvim-lua"
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
 
 	-- snippets
-	use "L3MON4D3/LuaSnip" -- snippet engine
-	use "rafamadriz/friendly-snippets" -- a bunch of snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets
 
 	-- lsp
-	use "neovim/nvim-lspconfig"
-	use "williamboman/nvim-lsp-installer" -- language server installer
-	use "jose-elias-alvarez/null-ls.nvim" -- linter
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer") -- language server installer
+	use("jose-elias-alvarez/null-ls.nvim") -- linter
 
 	-- telescope <C-q> to add to quickfix list
-	use "nvim-telescope/telescope.nvim"
+	use("nvim-telescope/telescope.nvim")
 
 	-- Treesitter
-	use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-	use "p00f/nvim-ts-rainbow"
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("p00f/nvim-ts-rainbow")
 
 	-- Autopairs
-	use "windwp/nvim-autopairs"
+	use("windwp/nvim-autopairs")
 
 	-- git
-	use "lewis6991/gitsigns.nvim"
+	use("lewis6991/gitsigns.nvim")
 
 	-- Undotree
-	use "mbbill/undotree"
+	use("mbbill/undotree")
+
+	-- Colourscheme
+	use("olimorris/onedarkpro.nvim")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
