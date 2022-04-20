60198808761f386d54bbff192e64e04afd514480
matt.walker@genesis.global <matt.walker@MACBOOK12.local>
2022-04-20 13:45:06 +0100

    Fixed config not working for vim cmp

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -53,8 +53,8 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -124,7 +124,7 @@ cmp.setup {
     select = false,
   },
 	window = {
-		documentation = "native"
+		documentation = cmp.config.window.bordered()
 	},
   experimental = {
     ghost_text = true,
