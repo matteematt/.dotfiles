d9ba5aae1afc80f59fb9f005f30978a8967de328
matt.walker@genesis.global <matt.walker@MACBOOK12.local>
2022-04-21 14:44:05 +0100

    Set the auto close tag keybinding to be <leader>>

--- config/nvim/lua/user/closetag.lua
+++ config/nvim/lua/user/closetag.lua
@@ -3,4 +3,20 @@ vim.cmd [[
 " These are the file types where this plugin is enabled.
 "
 let g:closetag_filetypes = 'html,xhtml,phtml,typescript'
+
+" dict
+" Disables auto-close if not in a "valid" region (based on filetype)
+" (This does not work with the template region from web components)
+" let g:closetag_regions = {
+"     \ 'typescript': 'jsxRegion,tsxRegion',
+"     \ 'javascript': 'jsxRegion',
+"     \ 'typescript.tsx': 'jsxRegion,tsxRegion',
+"     \ 'javascript.jsx': 'jsxRegion',
+"     \ 'typescriptreact': 'jsxRegion,tsxRegion',
+"     \ 'javascriptreact': 'jsxRegion',
+"     \ }
+
+" Shortcut for closing tags, default is '>'
+" Changing this because of <> also being used for templates/generics in languages like typescript
+let g:closetag_shortcut = '<leader>>'
 ]]
