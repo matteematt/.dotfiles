6c7449ba0c7874782862ed40e7a5a4f62fd1130a
Matt Walker <matt.walker@genesis.global>
2022-09-14 13:50:43 +0100

    Added rainbow_csv vim plugin

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -85,6 +85,9 @@ return require("packer").startup(function(use)
 	-- Automatically close (x)html tags
 	use("alvan/vim-closetag")
 
+	-- Syntax highlighting for csv files
+	use("mechatroner/rainbow_csv")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
