b5c89726eb62036fb773c78324550d8c35453a81
Matt Walker <matt.walker@genesis.global>
2022-12-23 10:44:52 +0000

    fix: onedark theme and lsp fixes

--- config/nvim/lua/user/colourscheme.lua
+++ config/nvim/lua/user/colourscheme.lua
@@ -11,54 +11,57 @@ local colourscheme = "onedarkpro"
 -- end
 
 if colourscheme == "onedarkpro" then
-	vim.o.termguicolors = true
-	vim.o.background = "dark" -- to load onedark
-	local onedark_config = {
-		-- Theme can be overwritten with 'onedark' or 'onelight' as a string
-		theme = function()
-			if vim.o.background == "dark" then
-				return "onedark"
-			else
-				return "onelight"
-			end
-		end,
-		colors = {
-			onedark = {
-				-- Vivid colors from https://github.com/Binaryify/OneDark-Pro
-				red = "#ef596f",
-				green = "#89ca78",
-				cyan = "#2bbac5",
-				purple = "#d55fde",
-			},
-		}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
-		highlights = {}, -- Override default highlight groups
-		plugins = { -- Override which plugins highlight groups are loaded
-			native_lsp = true,
-			polygot = true,
-			treesitter = true,
-			-- NOTE: Other plugins have been omitted for brevity
+
+	require("onedarkpro").setup({
+  colors = {
+		onedark = {
+			-- Vivid colors from https://github.com/Binaryify/OneDark-Pro
+			red = "#ef596f",
+			green = "#89ca78",
+			cyan = "#2bbac5",
+			purple = "#d55fde",
 		},
-		styles = {
-			strings = "NONE", -- Style that is applied to strings
-			comments = "italic", -- Style that is applied to comments
-			keywords = "bold", -- Style that is applied to keywords
-			functions = "bold", -- Style that is applied to functions
-			variables = "NONE", -- Style that is applied to variables
-		},
-		options = {
-			bold = true, -- Use the themes opinionated bold styles?
-			italic = true, -- Use the themes opinionated italic styles?
-			underline = true, -- Use the themes opinionated underline styles?
-			undercurl = true, -- Use the themes opinionated undercurl styles?
-			cursorline = true, -- Use cursorline highlighting?
-			transparency = true, -- Use a transparent background?
-			terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
-			window_unfocussed_color = false, -- When the window is out of focus, change the normal background?
-		},
-	}
-	local onedarkpro = require("onedarkpro")
-	onedarkpro.load(onedarkpro.setup(onedark_config))
+  }, -- Override default colors or create your own
+  highlights = {}, -- Override default highlight groups or create your own
+  filetypes = { -- Override which filetype highlight groups are loaded
+    -- See the 'Configuring filetype highlights' section for the available list
+  },
+  plugins = { -- Override which plugin highlight groups are loaded
+	native_lsp = true,
+	polygot = true,
+	treesitter = true,
+    -- See the 'Supported plugins' section for the available list
+  },
+  styles = { -- For example, to apply bold and italic, use "bold,italic"
+    types = "NONE", -- Style that is applied to types
+    numbers = "NONE", -- Style that is applied to numbers
+    strings = "NONE", -- Style that is applied to strings
+    comments = "italic", -- Style that is applied to comments
+    keywords = "bold", -- Style that is applied to keywords
+    constants = "NONE", -- Style that is applied to constants
+    functions = "bold", -- Style that is applied to functions
+    operators = "NONE", -- Style that is applied to operators
+    variables = "NONE", -- Style that is applied to variables
+    conditionals = "NONE", -- Style that is applied to conditionals
+    virtual_text = "NONE", -- Style that is applied to virtual text
+  },
+  options = {
+    bold = true, -- Use bold styles?
+    italic = true, -- Use italic styles?
+    underline = true, -- Use underline styles?
+    undercurl = true, -- Use undercurl styles?
+
+    cursorline = true, -- Use cursorline highlighting?
+    transparency = true, -- Use a transparent background?
+    terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+    highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+  }
+})
+
+vim.cmd("colorscheme onedark")
+
 elseif colourscheme == "gruvbit" then
+
 	vim.cmd([[
 	if (has("termguicolors"))
 		set termguicolors
