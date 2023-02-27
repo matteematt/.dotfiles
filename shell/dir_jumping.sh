b95f121e96fa535c715bb48136d7d10ae5f1711b
Matt Walker <matt.walker@genesis.global>
2023-02-27 11:32:23 +0000

    feat: bash commands for jumping around a git directory using fd and fzf

--- shell/dir_jumping.sh
+++ shell/dir_jumping.sh
@@ -7,14 +7,18 @@ function changeDirShortcut {
 
 # Saves the current directory in a history file, ordered by last access time,
 # removes duplicates, and keeps the previous 1000
+# ignores git worktree directories
 function __pushchangeddirToListSave {
+	# return from function if the current working directory contains _worktrees_git
+	if [[ $(pwd) == *"_worktrees_git"* ]] && return;
   cdhistory="$HOME/.cache/cdhistory"
   touch "$cdhistory"
 	echo "$(pwd | sed 's/ /<spc;>/') $(date +'%s')" >> "$cdhistory"
   sort -r "$cdhistory" | sort -k1,1 --unique | sort -k 2,2 | tail -n 1000 > "$cdhistory.tmp"
   mv -f "$cdhistory.tmp" "$cdhistory"
 }
-# After a normal 'cd' calls function to save dir in the background
+
+# After a normal 'cd' calls function to save dir in the background, unless in a worktree
 function pushChangedDirToList {
   cd "$1"
 	(__pushchangeddirToListSave &) &>/dev/null
@@ -23,7 +27,18 @@ function pushChangedDirToList {
 # Use fzf to choose a dir to jump to from the history
 function changeDirFromHistory {
   cdhistory="$HOME/.cache/cdhistory"
-	chosen_dir=$(rev "$cdhistory" | cut -d" " -f 2-  | rev | sed 's/<spc;>/ /' | fzf --tac)
+	chosen_dir=$(rev "$cdhistory" | cut -d" " -f 2-  | rev | sed 's/<spc;>/ /' | fzf --tac --header "History Jump")
+  if [[ -d "$chosen_dir" ]]; then
+		pushChangedDirToList "$chosen_dir"
+  fi
+}
+
+# Find all directories inside of a git project and jump to it
+# Works in a git repo or in a worktree
+function changeDirInsideGitProject {
+	top_level="$(git rev-parse --show-toplevel)"
+	chosen_dir=$(cd "$top_level" && fd --type directory | fzf --header "Project Jump")
+	chosen_dir="$top_level/$chosen_dir"
   if [[ -d "$chosen_dir" ]]; then
 		pushChangedDirToList "$chosen_dir"
   fi
