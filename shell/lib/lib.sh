2241e66dde21c2b40388020d6fab8125b09c905a
Matt Walker <43502076+matteematt@users.noreply.github.com>
2023-03-30 10:41:06 +0000

    feat(shell): project jump improvements (#34)
    * feat: add jumping to project root when using cdp
    * feat: colourize ls output when using cdp
    * chore: refactor out the coloured fzf preview call
    * chore: added comment about how ls viewer works

--- shell/lib/lib.sh
+++ shell/lib/lib.sh
@@ -13,4 +13,11 @@ confirm() {
     esac
 }
 
-echo "Sourced this file"
+# Use FZF to preview a list of directories using `eval` where you pipe in using <<< the directories stream
+# See usage in `changeDirInsideGitProject` as an example.
+# `CLICOLOR_FORCE=1` is needed to force color output in `ls`, along with `-G` for mac and `--color=auto` for linux
+# `--ansi` is needed to force color output in fzf reading the ANSI colour codes
+# `"$1"` sets the title shown in the fzf box.
+fzfLsPreview() {
+	echo "fzf --ansi --tac --header \"$1\" --preview 'CLICOLOR_FORCE=1 ls -al -G --color=auto {}'"
+}
