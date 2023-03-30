4c1024e1c8a0011f16e30be440c6998bf4d7611c
Matt Walker <matt.walker@genesis.global>
2023-03-30 16:22:43 +0100

    fix: cdp correctly displays the directory preview even when not in the root

--- shell/lib/lib.sh
+++ shell/lib/lib.sh
@@ -18,6 +18,7 @@ confirm() {
 # `CLICOLOR_FORCE=1` is needed to force color output in `ls`, along with `-G` for mac and `--color=auto` for linux
 # `--ansi` is needed to force color output in fzf reading the ANSI colour codes
 # `"$1"` sets the title shown in the fzf box.
+# `"$2"` optionally sets a directory to the text in fzf to preview (for example to prepend the project root).
 fzfLsPreview() {
-	echo "fzf --ansi --tac --header \"$1\" --preview 'CLICOLOR_FORCE=1 ls -al -G --color=auto {}'"
+	echo "fzf --ansi --tac --header \"$1\" --preview 'CLICOLOR_FORCE=1 ls -al -G --color=auto $2{}'"
 }
