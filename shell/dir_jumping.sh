5b9e1d34de1e457addb67c4108b865687edd4c2e
Matt Walker <matt.walker@genesis.global>
2023-04-03 11:00:44 +0100

    fix: worktree jump works inside of a worktree

--- shell/dir_jumping.sh
+++ shell/dir_jumping.sh
@@ -48,8 +48,8 @@ function changeDirInsideGitProject {
 
 # Find all branches inside of the projects _worktrees_git to jump to
 function changeWorktreeProject {
-	top_level="$(git rev-parse --show-toplevel)"
-	chosen_dir=$(find ./_worktrees_git -type d -exec test -e '{}/.git' ';' -print -prune | cut -c 18- | fzf --header "Worktree Jump" --preview "cd $top_level/_worktrees_git/{} && git log")
+	top_level="$(cd "$(pwd | awk -v FS="_worktrees_git/" '{print $1}')" && git rev-parse --show-toplevel)"
+	chosen_dir=$(cd "$top_level" && find ./_worktrees_git -type d -exec test -e '{}/.git' ';' -print -prune | cut -c 18- | fzf --header "Worktree Jump" --preview "cd $top_level/_worktrees_git/{} && git log")
 	chosen_dir="$top_level/_worktrees_git/$chosen_dir"
 	if [[ -d "$chosen_dir" ]]; then
 		pushChangedDirToList "$chosen_dir"
