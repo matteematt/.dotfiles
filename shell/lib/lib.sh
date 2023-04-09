6d69407e06dbf844c0e6f64df12780df5855351d
Matt Arch Linux <mattwalker340@gmail.com>
2023-04-09 13:42:29 +0100

    fix: correct call to ls to show colours on linux

--- shell/lib/lib.sh
+++ shell/lib/lib.sh
@@ -15,10 +15,10 @@ confirm() {
 
 # Use FZF to preview a list of directories using `eval` where you pipe in using <<< the directories stream
 # See usage in `changeDirInsideGitProject` as an example.
-# `CLICOLOR_FORCE=1` is needed to force color output in `ls`, along with `-G` for mac and `--color=auto` for linux
+# `CLICOLOR_FORCE=1` is needed to force color output in `ls`, along with `-G` for mac and `--color=always` for linux
 # `--ansi` is needed to force color output in fzf reading the ANSI colour codes
 # `"$1"` sets the title shown in the fzf box.
 # `"$2"` optionally sets a directory to the text in fzf to preview (for example to prepend the project root).
 fzfLsPreview() {
-	echo "fzf --ansi --tac --header \"$1\" --preview 'CLICOLOR_FORCE=1 ls -al -G --color=auto $2{}'"
+	echo "fzf --ansi --tac --header \"$1\" --preview 'CLICOLOR_FORCE=1 ls -al -G --color=always $2{}'"
 }
