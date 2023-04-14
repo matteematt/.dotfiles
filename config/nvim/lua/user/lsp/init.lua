713d19d741bac1c02f20f66dd252ca34869e5a83
Matt Walker <43502076+matteematt@users.noreply.github.com>
2023-04-14 10:45:28 +0000

    feat(nvim): message when lsp is ready (#43)

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -7,3 +7,23 @@ end
 require("user.lsp.mason")
 require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
+
+Filetypes_connected = {}
+
+vim.api.nvim_create_autocmd("LspTokenUpdate", {
+	callback = function(_)
+		local filetype = vim.bo.filetype
+		if Filetypes_connected[filetype] then
+			return
+		end
+		print("Lsp attatched for filetype: " .. filetype)
+		Filetypes_connected[filetype] = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspDetach", {
+	callback = function(args)
+		local filetype = vim.bo.filetype
+		Filetypes_connected[filetype] = false
+	end,
+})
