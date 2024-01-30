a46b3c76f1d518e4d7281cb234a7472b15b1b32d
Matt Arch Linux <mattwalker340@gmail.com>
2024-01-30 17:36:53 +0000

    arch backup cache but ignore yay cache

--- arch/maintenance.sh
+++ arch/maintenance.sh
@@ -24,7 +24,7 @@ brew update && brew upgrade
 echo "Backup installed packages list"
 pacman -Qqet > ~/.dotfiles/arch/pkglist.txt
 brew list --versions > ~/.dotfiles/arch/brewlist.txt
-cp -r $HOME/.cache $HOME/.cache.bak
+rsync -a --exclude='yay' $HOME/.cache $HOME/.cache.bak
 
 # Clear pacman cache, but keep the three most recent versions
 echo "Clearing pacman cache"
