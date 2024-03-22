1c28e9fbe648af2d60c2a61946ebccd8b68f2fe1
Matt Walker <43502076+matteematt@users.noreply.github.com>
2024-03-22 09:07:16 +0000

    Mac/maintenance 22 03 24 (#114)
    * chore(macos): maintenance
    * fix: backup .cache directory

--- mac/maintenance.sh
+++ mac/maintenance.sh
@@ -5,7 +5,7 @@ brew update && brew upgrade
 
 echo -e "\\nbackup files and package names"
 brew list --versions > ~/.dotfiles/mac/brewlist.txt
-cp -r $HOME/.cache $HOME/.cache.bak
+rsync -av  $HOME/.cache/ $HOME/.cache.backup/
 
 echo -e "\\nUpdate nvim packages with"
 echo "nvim $HOME/.dotfiles/config/nvim/lua/user/plugins.lua"
