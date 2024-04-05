a393386807cec9b39e442930b29326972a756fe3
Matt Walker <matt.walker@genesis.global>
2024-04-05 07:36:27 +0100

    feat: use public codeium for now

--- config/nvim/lua/user/codeium.lua
+++ config/nvim/lua/user/codeium.lua
@@ -5,20 +5,21 @@ if not status_ok then
 end
 
 -- if the working directory contains tmp or genesis then use the genesis copilot
-if vim.fn.getcwd():find("tmp") or vim.fn.getcwd():find("genesis") then
-	print("Codeium using genesis server")
-	codeium.setup({
-		enable_chat = true,
-		config_path = "/Users/matt.walker/.codeium.genesis.json",
-		api = {
-			portal_url = "copilot.genesislcap.com",
-			host = "copilot.genesislcap.com",
-			path = "/_route/api_server",
-		},
-	})
-else
+-- if vim.fn.getcwd():find("tmp") or vim.fn.getcwd():find("genesis") then
+	-- print("Codeium using genesis server")
+	-- codeium.setup({
+		-- enable_chat = true,
+		-- config_path = "/Users/matt.walker/.codeium.genesis.json",
+		-- api = {
+			-- portal_url = "copilot.genesislcap.com",
+			-- host = "copilot.genesislcap.com",
+			-- path = "/_route/api_server",
+		-- },
+		-- enterprise_mode = true
+	-- })
+-- else
 	print("Codeium using public server")
 	codeium.setup({
 		enable_chat = true,
 	})
-end
+-- end
