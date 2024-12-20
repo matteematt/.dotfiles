bcf289f4547d164cf7fc60e3c93bbf59d6d2ca91
Matt Walker <43502076+matteematt@users.noreply.github.com>
2024-12-20 08:16:23 +0000

    feat(macos): install script to update and configure llm (#164)

--- mac/maintenance.sh
+++ mac/maintenance.sh
@@ -7,5 +7,14 @@ echo -e "\\nbackup files and package names"
 brew list --versions > ~/.dotfiles/mac/brewlist.txt
 rsync -av  $HOME/.cache/ $HOME/.cache.backup/
 
+if command -v llm >/dev/null 2>&1; then
+    echo "Updating llm"
+		llm install llm-claude-3
+		llm models default claude-3-5-sonnet-latest
+else
+    echo "llm is not installed"
+fi
+
+
 echo -e "\\nUpdate nvim packages with"
 echo "nvim $HOME/.dotfiles/config/nvim/lua/user/plugins.lua"
