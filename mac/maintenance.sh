ae00ee8b8637d3f3ed97521167a3fcca42537821
Matt Walker <43502076+matteematt@users.noreply.github.com>
2025-02-14 10:16:08 +0000

    chore(macos): maintenance (#182)
    * chore(macos): maintenance
    * chore(macos): set default llm model gemeni

--- mac/maintenance.sh
+++ mac/maintenance.sh
@@ -9,8 +9,10 @@ rsync -av  $HOME/.cache/ $HOME/.cache.backup/
 
 if command -v llm >/dev/null 2>&1; then
     echo "Updating llm"
-		llm install llm-claude-3
-		llm models default claude-3-5-sonnet-latest
+		# llm install llm-claude-3
+		# llm models default claude-3-5-sonnet-latest
+		llm install llm-gemini
+		llm models default gemini-2.0-flash
 else
     echo "llm is not installed"
 fi
